415030000dcd7027b53fd892a6a30ff6fe182975
roshni090496 <rs09041001@gmail.com>
2022-06-13 09:48:33 -0700

    Added : Mood Analyzer Code

---
+++ src/com/company/MoodAnalyzer.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class MoodAnalyzer {
+    public String message;
+
+    //DEFAULT CONSTRUCTOR
+    public MoodAnalyzer() {
+
+    }
+
+    //PARAMETERISED CONSTRUCTOR
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    //METHOD TO ANALYZE MOOD
+    public String analyseMood() {
+        {
+            try {
+                if (message.contains("Sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+            return "HAPPY";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("******************** WELCOME TO MOOD ANALYSER ********************");
+    }
+}
+
---
+++ src/com/company/MoodAnalyzerTest.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest{
+        @Test
+        public void givenMessage_WhenMatchSad_ThenReturnSad()
+        {
+            MoodAnalyzer moodAnalyzer=new MoodAnalyzer("I am in Sad Mood");
+            String mood=moodAnalyzer.analyseMood();
+            Assert.assertEquals("SAD", mood);
+        }
+
+        @Test
+        public void givenMessage_WhenMatchHappy_ThenReturnHappy()
+        {
+            MoodAnalyzer moodAnalyzer=new MoodAnalyzer("I am in Happy Mood");
+            String mood=moodAnalyzer.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        }
+
+        @Test
+        public void givenMessage_WhenMatchNull_ThenReturnHappy()
+        {
+            MoodAnalyzer moodAnalyser=new MoodAnalyzer(null);
+            String mood=moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        }
+    }
+
